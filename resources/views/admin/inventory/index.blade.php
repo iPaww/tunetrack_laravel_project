80feaf47d560aaf1b920ed497a1b23979a0f10e6
Phaul Isaac Ado <157489613+iPaww@users.noreply.github.com>
2025-01-06 21:06:42 +0800

    Index

--- resources/views/admin/inventory/index.blade.php
+++ resources/views/admin/inventory/index.blade.php
@@ -1,24 +1,24 @@
 <?php
 use Illuminate\Support\Collection;
 ?>
-<div class="title col-sm-3">Inventory</div>
+<div class="title col-sm-3"><b>INVENTORY</b></div>
 
 <a class="btn btn-primary m-1" href="/admin/inventory/add">Add</a>
 
 <div>
     @if ($errors->any())
-    <ul class="list-group my-2">
-        @foreach ($errors->all() as $error)
-            <li class="list-group-item list-group-item-danger">{{ $error }}</li>
-        @endforeach
-    </ul>
+        <ul class="list-group my-2">
+            @foreach ($errors->all() as $error)
+                <li class="list-group-item list-group-item-danger">{{ $error }}</li>
+            @endforeach
+        </ul>
     @endif
     @if (session()->get('data'))
-    <ul class="list-group my-2">
-        @foreach (session()->get('data') as $data)
-            <li class="list-group-item list-group-item-success">{{ $data }}</li>
-        @endforeach
-    </ul>
+        <ul class="list-group my-2">
+            @foreach (session()->get('data') as $data)
+                <li class="list-group-item list-group-item-success">{{ $data }}</li>
+            @endforeach
+        </ul>
     @endif
 </div>
 <table class="table table-striped">
@@ -36,32 +36,37 @@ use Illuminate\Support\Collection;
             <tr>
                 <td class="text-start">
                     <a href="/admin/products/edit/{{ $product->id }}" class="text-decoration-none">
-                        <img class="img-thumbnail" src="{{ asset('assets/images/inventory/uploads/' . $product->image) }}" alt="inventory Image" width="100">
+                        <img class="img-thumbnail"
+                            src="{{ asset('assets/images/inventory/uploads/' . $product->image) }}"
+                            alt="inventory Image" width="100">
                         <span class="ms-2">{{ $product->name }}</span>
                     </a>
                 </td>
                 <td>{{ $product->type->name }}</td>
                 <td>
-                    @if ( $product->product_type_id == 1 )
+                    @if ($product->product_type_id == 1)
                         <ul>
-                            @foreach( collect(explode(',', $product->color_names))->unique()->sort()->all() as $color_variant)
+                            @foreach (collect(explode(',', $product->color_names))->unique()->sort()->all() as $color_variant)
                                 <li class="text-start">{{ $color_variant }}</li>
                             @endforeach
                         </ul>
-                    @elseif ( $product->product_type_id == 2 )
+                    @elseif ($product->product_type_id == 2)
                         {{ $product->color_name }}
                     @endif
                 </td>
                 <td>
-                    @if ( $product->product_type_id == 1 )
+                    @if ($product->product_type_id == 1)
                         {{ $product->products_quantity }}
-                    @elseif ( $product->product_type_id == 2 )
+                    @elseif ($product->product_type_id == 2)
                         {{ $product->quantity }}
                     @endif
                 </td>
                 <td class="text-nowrap">
-                    <a href="/admin/inventory/edit/{{ $product->id }}/product-type/{{ $product->product_type_id }}/color/{{ $product->color_id }}" class="btn btn-primary btn-sm">Edit</a>
-                    <form action="/admin/inventory/delete/{{ $product->id }}/product-type/{{ $product->product_type_id }}/color/{{ $product->color_id }}" method="POST" style="display:inline-block;">
+                    <a href="/admin/inventory/edit/{{ $product->id }}/product-type/{{ $product->product_type_id }}/color/{{ $product->color_id }}"
+                        class="btn btn-primary btn-sm">Edit</a>
+                    <form
+                        action="/admin/inventory/delete/{{ $product->id }}/product-type/{{ $product->product_type_id }}/color/{{ $product->color_id }}"
+                        method="POST" style="display:inline-block;">
                         @csrf <!-- {{ csrf_field() }} -->
                         @method('DELETE')
                         <button type="submit" class="btn btn-danger btn-sm" title="Delete">Delete</button>
--- resources/views/admin/quiz/index.blade.php
+++ resources/views/admin/quiz/index.blade.php
@@ -1,4 +1,4 @@
-<div class="container mt-5">
+<div class="container">
     <h1>Quiz</h1>
     <button class="btn btn-primary"><a href="/admin/quiz/add" style="text-decoration: none; color:white;">Add</a></button>
     <table class="table table-striped">
@@ -15,18 +15,21 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($quizzes as $quiz) <!-- Use $quizzes instead of $Quiz -->
+            @foreach ($quizzes as $quiz)
+                <!-- Use $quizzes instead of $Quiz -->
                 <tr>
-                    <td>{{ $quiz->course->name ?? 'No Course Assigned' }}</td> <!-- Assuming 'course' is the relationship to Course model -->
+                    <td>{{ $quiz->course->name ?? 'No Course Assigned' }}</td>
+                    <!-- Assuming 'course' is the relationship to Course model -->
                     <td>{{ $quiz->question ?? 'No Course Assigned' }}</td>
                     <td>{{ $quiz->a_answer ?? 'No Course Assigned' }}</td>
                     <td>{{ $quiz->b_answer ?? 'No Course Assigned' }}</td>
                     <td>{{ $quiz->c_answer ?? 'No Course Assigned' }}</td>
                     <td>{{ $quiz->c_answer ?? 'No Course Assigned' }}</td>
                     <td>{{ $quiz->correct_answer ?? 'No Course Assigned' }}</td>
-                    <td class="text-nowrap" >
+                    <td class="text-nowrap">
                         <a href="{{ route('quiz.edit', $quiz->id) }}" class="btn btn-primary btn-sm">Edit</a>
-                        <form action="{{ route('quiz.destroy', $quiz->id) }}" method="POST" style="display:inline-block;">
+                        <form action="{{ route('quiz.destroy', $quiz->id) }}" method="POST"
+                            style="display:inline-block;">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger btn-sm" title="Delete">Delete</button>
