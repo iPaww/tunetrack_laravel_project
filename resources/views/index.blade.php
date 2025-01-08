e3874734ed9ea01bb3367eb1a760f76cae8b9257
Mico Amorganda <147843989+NuisanceMics@users.noreply.github.com>
2025-01-09 00:51:25 +0800

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,5 @@
 <!-- Centered content with titles and search bar -->
 <div class="centered-content text-center">
-    <form action="{{ route('course.search', ['course' => $course->id]) }}" method="GET" class="d-flex w-50">
-        <input type="text" name="query" class="form-control" placeholder="Search Topics..." value="{{ request()->query('query') }}" aria-label="Search Topics">
-        <button type="submit" class="btn btn-primary ms-2">Search</button>
-    </form>
+    <div class="title-home">Learn Instruments</div>
+    <div class="title-home-2">With TuneTrack!</div>
 </div>
