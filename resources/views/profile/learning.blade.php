975ca14df25f6af0d9c93c88c88cd7245a20ba95
Mico Amorganda <147843989+NuisanceMics@users.noreply.github.com>
2025-01-13 21:31:58 +0800

    learning path update

--- resources/views/profile/learning.blade.php
+++ resources/views/profile/learning.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
     <h4>My Learning</h4>
     <div class="row">
-        @if (count($courses_history) > 0)
+        @if( count($courses_history) > 0 )
             @foreach ($courses_history as $course_history)
                 <div class="col-xl-3 col-md-4 col-sm-6 mb-md-3">
                     <div class="card h-100">
@@ -9,15 +9,17 @@
                             <h5 class="card-title">{{ $course_history->course->name }}</h5>
                             <p class="text-truncate">{{ $course_history->course->description }}</p>
                             <div class="h-25">
-                                <div class="progress" role="progressbar" aria-label="Warning example"
-                                    aria-valuenow="{{ ceil($course_history->topics_viewed / ($course_history->course->topics->count() || 1)) }}"
-                                    aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress" role="progressbar" aria-label="Warning example" aria-valuenow="{{ ceil( $course_history->topics_viewed / ($course_history->course->topics->count() || 1)) }}" aria-valuemin="0" aria-valuemax="100">
                                     <div @class([
                                         'progress-bar' => true,
                                         'bg-success' => ceil( ($course_history->topics_viewed / ($course_history->course->topics->count() ?? 1 )) * 100 ) >= 100,
                                         'bg-warning' => ceil( ($course_history->topics_viewed / ($course_history->course->topics->count() ?? 1 )) * 100 ) < 100,
                                     ]) style="width: {{ ceil( ($course_history->topics_viewed / ($course_history->course->topics->count() ?? 1 )) * 100 ) }}%"></div>
                                 </div>
+                                {{$course_history->course->topics->count()}}
+                                {{$course_history->topics_viewed}}
+                                {{($course_history->course->topics->count() || 1)}}
+                                <?= $course_history->topics_viewed >= ($course_history->course->topics->count() ?? 1)?>
                                 @if ( $course_history->topics_viewed >= ($course_history->course->topics->count() ?? 1) )
                                     <div class="text-center">
                                         <small>
@@ -27,15 +29,13 @@
                                 @else
                                     <div class="text-center">
                                         <small>
-                                            In progress, You have finished {{ $course_history->topics_viewed }} out of
-                                            {{ $course_history->course->topics->count() }} topics
+                                            In progress, You have finished {{ $course_history->topics_viewed }} out of {{ $course_history->course->topics->count() }} topics
                                         </small>
                                     </div>
                                 @endif
                             </div>
-                            <a class="btn btn-tunetrack w-100 mt-3"
-                                href="/elearning/category/{{ $course_history->course->category_id }}/course/{{ $course_history->course->id }}">
-                                @if ($course_history->topics_viewed >= $course_history->course->topics->count())
+                            <a class="btn btn-tunetrack w-100 mt-3" href="/elearning/category/{{ $course_history->course->category_id }}/course/{{ $course_history->course->id }}">
+                                @if ( $course_history->topics_viewed >= $course_history->course->topics->count() )
                                     See more
                                 @else
                                     Continue
@@ -46,13 +46,11 @@
                 </div>
             @endforeach
         @else
-            <div class="col-12">
-                <p class="text-center fs-4">
-                    You did not start any quiz yet, please go to our <a href="/elearning"
-                        class="text-decoration-none">eLearning</a> section and a new course or continue <a
-                        href="/profile/learning" class="text-decoration-none">courses</a>.
-                </p>
-            </div>
+        <div class="col-12">
+            <p class="text-center fs-4">
+                You did not start any quiz yet, please go to our <a href="/elearning" class="text-decoration-none">eLearning</a> section and a new course or continue <a href="/profile/learning" class="text-decoration-none">courses</a>.
+            </p>
+        </div>
         @endif
         <div class="col-12">
             {{ $courses_history->links() }}
