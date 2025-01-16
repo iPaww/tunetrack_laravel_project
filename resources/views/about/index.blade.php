73b0f4fe0ff5eaae1726e0af29d7b510ba70f858
Mico Amorganda <147843989+NuisanceMics@users.noreply.github.com>
2025-01-16 23:04:01 +0800

    about us

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,25 @@
+<div class="container py-4">
+    <div class="mb-3">
+        <h1 class="text-center fs-1">About Us</h1>
+        <div class="text-center">
+            <img src="{{ asset('assets/images/about/default.png') }}"
+                class="img-fluid border rounded"
+                alt="about store image"
+                style="max-width: 100%; max-height: 500px; object-fit: cover;" />
+        </div>
+    </div>
+    <div>
+        <h1 class="text-center fs-1">Contact Us</h1>
+        <div class="row">
+            <div class="col-12">
+                <p class="text-center fs-4">You’re welcome to contact us with any inquiry</p>
+            </div>
+            <div class="col-12">
+                <div class="text-center">
+                    <p class="mb-0">no: 1234-567-8910</p>
+                    <p class="mb-0">jcsmusicstore@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
